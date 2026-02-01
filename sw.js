3afdf49cc1027b3547ecc40d3d75325f21ba476d
Da Wei <dvs1682001@gmail.com>
2026-02-01 23:13:20 +0800

    V1.0

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'elderly-meds-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap'
+];
+
+// 安裝時快取檔案
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 攔截請求，優先從快取讀取（達到離線使用）
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
+// 清理舊快取
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
